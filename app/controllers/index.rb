8cdef5cd2ba9f1817b05d26b9690fd839d10e1fa
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-01 16:54:08 -0500

    fixing pull from master

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -23,30 +23,37 @@ end
 
 #################    CHASM OF DOOOOOOOOOOOOMMMMMM        ########################
 
+#Chirag and Meara's side!!!!
 
 get '/' do
   # list decks
   # decks are clickable
   # deck links to post with deck_id
 
+  @decks = Deck.all
 
   erb :index
 end
 
 post '/deck/:deck_id/round/' do
-  # deck = Deck.find_by_id(params[:deck_id])
-  
+  deck = Deck.find_by_id(params[:deck_id])
+  # Will have to grab user from session once that is implemented
+  user = User.find_by_id(1)
   # Create a round 
-  # round = deck.round.create()
-
+  round = user.rounds.create(deck_id: deck.id)
+  round.created_at.inspect
   # Get card from deck
-
+  card = deck.cards.first
   # Redirect to get round/roundid/card/cardid
+  redirect to("/round/#{round.id}/card/#{card.id}")
 end
 
 
 
 get '/round/:round_id/stats' do
+
+  @round = Round.find(params[:round_id])
+
   erb :stats_view
 
 end 
