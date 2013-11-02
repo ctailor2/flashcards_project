e573739ace69b5a30c8afd8e38885d882a1e7046
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-02 18:40:09 -0500

    case/punctuation insensitive answer check, moved some logic to helper methods, added autofocus for card nav

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,17 +7,13 @@ get '/round/:round_id/card/:card_id' do
   @card = Card.find_by_id(params[:card_id])
     erb :card_view
   end
+  
 end
 
 post '/round/:round_id/card/:card_id' do
   attempt = Attempt.create(round_id: params[:round_id], card_id: params[:card_id], guess: params[:guess])
   corresponding_card = Card.find_by_id(params[:card_id])
   check_answer(corresponding_card, attempt)
-  # if attempt.guess == corresponding_card.answer
-  #   Attempt.update(attempt.id, correct: true)
-  # else
-  #   Attempt.update(attempt.id, correct: false)
-  # end
   redirect to "/round/#{attempt.round_id}/card/#{attempt.card_id}/attempt/#{attempt.id}"
 end
 
@@ -42,8 +38,6 @@ end
 
 #################    CHASM OF DOOOOOOOOOOOOMMMMMM        ########################
 
-#Chirag and Meara's side!!!!
-
 get '/' do
   if logged_in?
     @decks = Deck.all
@@ -55,28 +49,16 @@ end
 
 post '/deck/:deck_id/round/' do
   deck = Deck.find_by_id(params[:deck_id])
-
   user = current_user
-  # Create a round 
   round = user.rounds.create(deck_id: deck.id)
-  round.created_at.inspect
-  # Get card from deck
   card = deck.cards.sample
-  # Redirect to get round/roundid/card/cardid
   redirect to("/round/#{round.id}/card/#{card.id}")
 end
 
 
 
 get '/round/:round_id/stats' do
-
   @round = Round.find(params[:round_id])
-
   erb :stats_view
-
 end 
 
-# ~> NoMethodError
-# ~> undefined method `get' for main:Object
-# ~>
-# ~> /Users/apprentice/Desktop/flashcards_project/app/controllers/index.rb:1:in `<main>'
