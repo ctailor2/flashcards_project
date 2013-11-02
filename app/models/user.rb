4bb43b9a2022400673a52bb74e5b03e3d3491135
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-02 16:37:40 -0500

    added validations to users and default for attempts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   has_many :rounds
   has_many :attempts, through: :rounds
+  validates :name, :email, :password, presence: true 
   validates :email, :uniqueness => true
+
   # May also want to add index to migration to enforce uniqueness.
 
   def self.authenticate(email, password)
