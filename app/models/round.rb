3367adfc266a07a526b06eec86293d0029a9acf0
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-02 16:38:32 -0500

    added stats methods to round model, also a deck method to return the deck for a round.  fleshed out stats view.

--- app/models/round.rb
+++ app/models/round.rb
@@ -7,10 +7,38 @@ class Round < ActiveRecord::Base
   def complete?
     # if number of attempts in round is same or larger than number of cards in the deck
     # number_of_attempts = self.attempts.count
-    number_of_cards_in_deck = Deck.find_by_id(self.deck_id).cards.count
+    number_of_cards_in_deck = deck.cards.count
     # number_of_attempts >= number_of_cards_in_deck
 
     Attempt.where(round_id: self.id, correct: true).length >= number_of_cards_in_deck
 
   end
+
+  def deck
+    Deck.find_by_id(self.deck_id)
+  end
+
+  def num_correct_attempts
+    Attempt.where(round_id: self.id, correct: true).length
+  end
+
+  def num_incorrect_attempts
+    nils = Attempt.where(round_id: self.id, correct: nil).length
+    falses = Attempt.where(round_id: self.id, correct: false).length
+    nils + falses
+  end
+
+  def num_total_attempts
+    num_correct_attempts + num_incorrect_attempts
+  end
+
+  def percent_correct
+    decimal = num_correct_attempts.to_f/num_total_attempts
+    percentage = (decimal * 100).round(2)
+  end
+
+  def avg_num_guesses
+    (num_total_attempts.to_f / deck.cards.count).round(2)
+  end
+
 end
