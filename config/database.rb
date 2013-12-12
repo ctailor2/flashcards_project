5320f67af1304caf83d59b35a75b78a442f7ccbd
Chirag Tailor <chiragtailor@Chirags-MacBook-Pro.local>
2013-12-12 09:09:46 -0600

    Comment out username/password from postgresql config file

--- config/database.rb
+++ config/database.rb
@@ -36,8 +36,8 @@ ActiveRecord::Base.establish_connection(
   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
   :host     => db.host,
   :port     => db.port,
-  :username => db.user,
-  :password => db.password,
+  # :username => db.user,
+  # :password => db.password,
   :database => DB_NAME,
   :encoding => 'utf8'
 )
